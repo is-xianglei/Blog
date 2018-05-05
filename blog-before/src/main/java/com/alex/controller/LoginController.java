98313c643fa8c9c14c5c35050c4d731225edb1cf
zhangzhe <zhangzhe_1518@163.com>
2018-05-05 13:26:21 +0800

    用户退出

--- blog-before/src/main/java/com/alex/controller/LoginController.java
+++ blog-before/src/main/java/com/alex/controller/LoginController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 /**
  * 用户登陆
@@ -58,6 +59,17 @@ public class LoginController {
         request.getSession().setAttribute("user",userResult);
         return "login";
 
+    }
+
+    /**
+     * 用户退出
+     * @param session
+     * @return
+     */
+    @RequestMapping(value = "/loginOut")
+    public String loginOut(HttpSession session){
+        session.removeAttribute("user");
+        return "login";
     }
 
 }
